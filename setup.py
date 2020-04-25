df59ef13f52c4428b822535cf2bebb1240f19227
Philipp <phil80301@berkeley.edu>
2020-04-24 20:39:58 -0700

    python setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="opthex",
+    version="0.0",
+    author="brent, philipp",
+    author_email="brentyi@berkeley.edu",
+    license="MIT",
+    description="opthex",
+    url="https://github.com/brentyi/opthex",
+    packages=find_packages(exclude=["tests"]),
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "mujoco_py",
+    ]
+)
